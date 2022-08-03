71c62b1f4e4cac22c9d7936477d00dfbf598da24
polva98 <jackpolva27@gmail.com>
2022-08-03 19:54:40 +0200

    estimate node activation probabilities function

--- Step2/Learner.py
+++ Step2/Learner.py
@@ -52,3 +52,47 @@ class Learner:
 
         return stop
     """
+
+    def estimate_node_activation_fully_connected(self, graph_probabilities, feature_probabilities):
+
+        tot_node_arrivals = np.empty((3, 5))  # z_i
+        counters = np.zeros(3)  # k_i
+        n_simulation = 10
+
+        for s in range(n_simulation):
+            user_type_index_0 = np.random.binomial(1, feature_probabilities[0])
+            user_type_index_1 = 0
+            if user_type_index_0:
+                user_type_index_1 = np.random.binomial(1, feature_probabilities[1])
+                edge_weights = graph_probabilities[user_type_index_0 + user_type_index_1]
+                counters[user_type_index_0 + user_type_index_1] += 1
+            else:
+                edge_weights = graph_probabilities[user_type_index_0]
+                counters[user_type_index_0] += 1
+
+            flat_edge_weights = np.ravel(edge_weights)
+            live_edge_graph = np.array([np.random.binomial(1, p) for p in flat_edge_weights])
+            live_edge_graph = np.reshape(live_edge_graph, (5, 5))
+
+            # DFS
+            frontier = []
+            already_visited = np.zeros(5)
+            seed = np.random.choice(5, 1).squeeze()
+
+            already_visited[seed] = 1
+            frontier.extend([idx for idx, p in enumerate(live_edge_graph[seed]) if p > 0])  # argwhere problem
+
+            while len(frontier) > 0:
+                seed = frontier.pop(0)
+                if already_visited[seed] > 0:
+                    continue
+                already_visited[seed] = 1
+                frontier.extend([idx for idx, p in enumerate(live_edge_graph[seed]) if p > 0])
+
+            if user_type_index_0:
+                tot_node_arrivals[user_type_index_0 + user_type_index_1] += already_visited
+            else:
+                tot_node_arrivals[user_type_index_0] += already_visited
+
+        node_arrival_probabilities = np.array([tot_node_arrivals[i] / count for i, count in enumerate(counters)])
+        return node_arrival_probabilities

9e448f4ef27b97cedbb11fd32c517ead03c8bf4e
RikyPala <riccardo.pala98@gmail.com>
2022-08-22 18:11:51 +0200

    update

--- Step2/Learner.py
+++ Step2/Learner.py
@@ -8,90 +8,115 @@ class Learner:
 
         self.env = env
         self.node_probabilities = []
-        self.landing_probabilities = []
 
-    # TODO: add function to estimate node-arrival probabilities
+    def greedy_optimization(self):
 
-    # TODO: add function to estimate expected rewards
+        iteration = 0
+        current_configuration = [0] * self.env.n_products
 
-    def greedy_optimization(self):  # TODO: remove pull_arms function
+        best_configuration = current_configuration
+        best_reward = self.evaluate_configuration(current_configuration)
 
-        config = [0] * self.env.n_products
-
-        self.node_probabilities = []
-        # for i in range(self.env.n_user_types):
-        self.node_probabilities = self.estimate_node_activation_fully_connected(self.env.graph_probabilities, self.env.feature_probabilities)
-        self.landing_probabilities = self.estimate_landing_probabilities(self.env.alpha_ratios_parameters)
-
-        best_configuration = config
-        best_reward = self.evaluate_configuration(config)
-        print(config)
-        print(best_reward)
-
-        while any(x < (self.env.n_arms - 1) for x in config):
-            print()
-            new_config = []
+        while any(x < (self.env.n_arms - 1) for x in current_configuration):
+            print(f"Best configuration n{iteration}: ", best_configuration)
+            print(f"Best reward n{iteration}: ", best_reward, "\n")
             for i in range(self.env.n_products):
-                new_config = config.copy()
-                if new_config[i] >= (self.env.n_arms - 1):
+                new_configuration = current_configuration.copy()
+                if new_configuration[i] >= (self.env.n_arms - 1):
                     continue
-                new_config[i] += 1
-                print(new_config)
-                reward = self.evaluate_configuration(new_config)
-                print(reward)
+                new_configuration[i] += 1
+                print("Testing configuration ", new_configuration, "...")
+                reward = self.evaluate_configuration(new_configuration)
+                print("Obtained reward:  ", reward)
                 if reward > best_reward:
-                    best_configuration = new_config
+                    best_configuration = new_configuration
                     best_reward = reward
                     print("# BEST")
-            if np.array_equal(config, best_configuration):
+            if np.array_equal(current_configuration, best_configuration):
                 break
-            config = best_configuration
+            current_configuration = best_configuration
+            iteration += 1
+            print()
 
-        print("\n########")
-        print(best_configuration)
-        print(best_reward)
+        print("#############\n")
+        print("Best configuration: ", best_configuration)
+        print("Best reward: ", best_reward)
+
         return best_configuration
 
-    def evaluate_configuration(self, config):
+    def evaluate_configuration(self, configuration):
+
+        daily_users = np.random.randint(10, 200)
+        reward = 0
+        alpha_ratios = self.env.draw_alpha_ratios()
+
+        for _ in range(daily_users):
+
+            user_type = self.env.draw_user_type()
+            product = self.env.draw_starting_page(user_type=user_type, alpha_ratios=alpha_ratios)
+            if product == 5:  # competitors' page
+                continue
+            visited = []
+            to_visit = [product]
+
+            while to_visit:
+                current_product = to_visit.pop(0)
+                visited.append(current_product)
+
+                product_price = self.env.prices[configuration[current_product]]
+
+                buy = np.random.binomial(1, self.env.conversion_rates[
+                    configuration[current_product], current_product, user_type])
+                if not buy:
+                    continue
+
+                products_sold = np.random.randint(0, self.env.max_products_sold[current_product, user_type])
+                reward += product_price * products_sold
+
+                secondary_1 = self.env.secondaries[current_product, 0]
+                success_1 = np.random.binomial(1, self.env.graph_probabilities[user_type, current_product, secondary_1])
+                if success_1 and secondary_1 not in visited and secondary_1 not in to_visit:
+                    to_visit.append(secondary_1)
+
+                secondary_2 = self.env.secondaries[current_product, 1]
+                success_2 = np.random.binomial(
+                    1, self.env.lambda_p * self.env.graph_probabilities[user_type, current_product, secondary_2])
+                if success_2 and secondary_2 not in visited and secondary_2 not in to_visit:
+                    to_visit.append(secondary_2)
+
+        return reward / daily_users
+
+    """
+    def evaluate_configuration2(self, configuration):
 
         reward = 0
 
         for prod in range(self.env.n_products):
-            arm = config[prod]
-            price = self.env.prices[config[prod]]
+            arm = configuration[prod]
+            price = self.env.prices[configuration[prod]]
             for user in range(self.env.n_user_types):
-                visits = self.env.user_probabilities[user] * self.landing_probabilities[user][prod + 1] * \
-                         self.node_probabilities[user][prod]
+                visits = self.env.user_probabilities[user] * self.node_probabilities[user][prod]
                 conversions = visits * self.env.conversion_rates[arm][prod][user]
-                sales = conversions * self.env.max_products_sold[prod][user]
+                sales = conversions * self.env.max_products_sold[prod][user] / 2
                 reward += sales * price
 
         return reward
 
-
     def estimate_node_activation_fully_connected(self, graph_probabilities, feature_probabilities):
 
-        tot_node_arrivals = np.empty((3, 5))  # z_i
-        counters = np.zeros(3)  # k_i
-        n_simulation = 10
-        n_day = 1
-        for d in range(n_day):
+        tot_node_arrivals = np.empty((3, 5))
+        tot_users = np.zeros(3)
+        days = 100
+        users_per_day = 10
+
+        for d in range(days):
 
             alpha_ratios = self.env.draw_alpha_ratios()
 
-            for s in range(n_simulation):
-                user_type_index_0 = np.random.binomial(1, feature_probabilities[0])
-                user_type_index_1 = 0
-                if user_type_index_0:
-                    user_type_index_1 = np.random.binomial(1, feature_probabilities[1])
-                    edge_weights = graph_probabilities[user_type_index_0 + user_type_index_1]
-                    counters[user_type_index_0 + user_type_index_1] += 1
-                    seed = self.env.draw_starting_page(user_type_index_0 + user_type_index_1, alpha_ratios)
-
-                else:
-                    edge_weights = graph_probabilities[user_type_index_0]
-                    counters[user_type_index_0] += 1
-                    seed = self.env.draw_starting_page(user_type_index_0, alpha_ratios)
+            for user in range(users_per_day):
+                user_type = self.env.draw_user_type()
+                tot_users[user_type] += 1
+                seed = self.env.draw_starting_page(user_type, alpha_ratios)
 
                 if seed == 5:
                     continue
@@ -113,17 +138,8 @@ class Learner:
                     already_visited[seed] = 1
                     frontier.extend([idx for idx, p in enumerate(live_edge_graph[seed]) if p > 0])
 
-                if user_type_index_0:
-                    tot_node_arrivals[user_type_index_0 + user_type_index_1] += already_visited
-                else:
-                    tot_node_arrivals[user_type_index_0] += already_visited
+                tot_node_arrivals[user_type] += already_visited
 
         node_arrival_probabilities = np.array([tot_node_arrivals[i] / count for i, count in enumerate(counters)])
         return node_arrival_probabilities
-
-    def estimate_landing_probabilities(self, params):
-
-        probs = params[:, :, 0] / (params[:, :, 0] + params[:, :, 1])
-        for a in probs:
-            a /= a.sum()
-        return probs
+    """
--- Step2/main.py
+++ Step2/main.py
@@ -1,62 +1,7 @@
-import numpy as np
-from tqdm.auto import tqdm
-from matplotlib import pyplot as plt
 from Environment import Environment
 from Learner import Learner
 
-n_experiments = 1000
-
-rewards_per_experiment = []
 env = Environment()
+learner = Learner(env)
 
-for e in tqdm(range(n_experiments)):
-
-    learner = Learner(env.n_products, env.n_arms, env.n_user_types)
-
-    while True:
-        configurations = learner.pull_arms()
-        for pulled_arms in configurations:
-            rewards = env.round(pulled_arms)
-            stop = learner.update(pulled_arms, rewards)
-            if stop:
-                break
-
-    rewards_per_experiment.append(learner.collected_rewards)
-
-plt.figure(0)
-plt.xlabel("t")
-plt.ylabel("Rewards")
-plt.figure(1)
-plt.xlabel("t")
-plt.ylabel("Cumulative Rewards")
-plt.figure(2)
-plt.xlabel("t")
-plt.ylabel("Cumulative Regrets")
-
-colors = ['g', 'b', 'r', 'y', 'm']
-
-for i in range(env.n_products):
-    product_experiments = [elem[i] for elem in rewards_per_experiment]
-    max_horizon = max([len(elem) for elem in product_experiments])
-    horizon = max_horizon
-    mean_rewards_per_horizon = []
-    mean_regrets_per_horizon = []
-    for t in range(max_horizon):
-        mean_rewards_per_horizon.append(np.mean([elem[t] for elem in product_experiments if len(elem) > t]))
-        mean_regrets_per_horizon.append(np.mean([env.optimals[i] - elem[t] for elem in product_experiments if len(elem) > t]))
-    plt.figure(0)
-    ax, = plt.plot(mean_rewards_per_horizon, colors[i])
-    ax.set_label('Product ' + str(i+1))
-    plt.figure(1)
-    ax, = plt.plot(np.cumsum(mean_rewards_per_horizon), colors[i])
-    ax.set_label('Product ' + str(i+1))
-    plt.figure(2)
-    ax, = plt.plot(np.cumsum(mean_regrets_per_horizon), colors[i])
-    ax.set_label('Product ' + str(i+1))
-plt.figure(0)
-plt.legend()
-plt.figure(1)
-plt.legend()
-plt.figure(2)
-plt.legend()
-plt.show()
+learner.greedy_optimization()

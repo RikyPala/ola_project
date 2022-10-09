14913d5220dc8c5f7c00d718686d538d3047bc91
AlessandroImmordino98 <aleimmordino@gmail.com>
2022-10-09 18:25:00 +0200

    Update

--- Step3_v3/UCB.py
+++ Step3_v3/UCB.py
@@ -1,43 +1,52 @@
 import numpy as np
-
 from Learner import Learner
-
-
-class UCBArm:
-
-    def __init__(self, c_value):
-        self.n = 0  # the number of times this arm has been pulled
-        self.x = []  # list of all rewards ?
-        self.empirical_means = 0
-        self.confidence = np.inf
-        self.c = c_value
-
-    def sample(self):
-        """ compute upper confidence bound """
-        return self.empirical_means + self.c * self.confidence
-
-    def update_empirical_means(self, reward):
-        self.n += 1
-        self.x.append(reward)  # needed?
-        self.empirical_means = (self.empirical_means * (self.n - 1) + reward) / self.n  # n or t?
-
-    def update_confidence(self, t):
-        self.confidence = (2 * np.log(t) / self.n) ** 0.5 if self.n > 0 else np.inf
+from  Environment import Environment, RoundData
 
 
 class UCB(Learner):
 
-    def __init__(self, arms_shape, c_value=200):
-        arms = []
-        n_arms = np.zeros(arms_shape).size
-        for _ in range(n_arms):
-            arms.append(UCBArm(c_value))
-        arms = np.array(arms).reshape(arms_shape)
-        super().__init__(arms)
+    def __init__(self, env: Environment):
+        super().__init__(env)
+        self.empirical_means = np.zeros((self.n_products, self.n_arms))
+        self.confidence = np.ones((self.n_products, self.n_arms)) * np.inf
+        self.pulled_rounds = np.zeros((self.n_products, self.n_arms))
 
-    def update(self, configuration, reward):
-        self.t += 1
-        self.arms[configuration].update_empirical_means(reward)
-        for arm in self.arms:
-            arm.update_confidence(self.t)
+    def update(self, results: RoundData):
+
+        configuration = results.configuration
+        for i in range(self.n_products):
+            self.empirical_means[i][configuration[i]] = (self.empirical_means[i][configuration[i]] *
+                                                       (self.pulled_rounds[i][configuration[i]]-1) + np.nan_to_num(results.conversions[i]/results.visits[i])) / self.pulled_rounds[i][configuration[i]]
+        for i in range(self.n_products):
+            for a in range(self.n_arms):
+                n_samples = self.pulled_rounds[i][a]
+                self.confidence[i][a] = 500*(2*np.log(self.t+1)/n_samples)**0.5 if n_samples > 0 else np.inf
+
+
+    def sample(self, results: RoundData):
+
+        superarm = []
+        for i in range(self.n_products):
+            dot = self.empirical_means[i] * self.prices[i]
+            dot = dot * self.products_sold[i]
+            if self.t > 1:
+                print("PREVIOUS PULLED ARM")
+                print(self.previous_pulled[i])
+                print("NODE PRBABILITIES")
+                print(self.node_prob[0][i])
+                dot[self.previous_pulled[i]] += self.marginal_reward[i][self.previous_pulled[i]]
+                print("FINALL DOTTT")
+                print(dot)
+                dot = dot + self.confidence[i]
+            j = np.random.choice(np.where(dot == dot.max())[0])
+            self.pulled_rounds[i][j] += 1
+            superarm.append(j)
+        print("PULLED ROUNDS")
+        print(self.pulled_rounds)
+        self.previous_pulled = superarm
+        print("SUPERARM")
+        print(superarm)
+        return superarm
+
+
 

394e6e744a09cb38144f84863f913f7c6ea67858
AlessandroImmordino98 <aleimmordino@gmail.com>
2022-10-10 14:19:20 +0200

    UCB implementation and methods for Learner to compute contributions

--- Step3_v3/Learner.py
+++ Step3_v3/Learner.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 
 import numpy as np
 
-from Step3_v3.Environment import Environment
+from Step3_v3.Environment import Environment, RoundData
 
 
 class Learner:
@@ -21,16 +21,19 @@ class Learner:
             env.graph_probabilities * np.expand_dims(env.user_probabilities, axis=(1, 2)),
             axis=0
         )
+        self.n_simulations = 100
+        self.marginal_rewards = np.zeros(env.n_products, env.n_arms)
 
-    def pull(self):
-        best_configuration = np.zeros(self.n_products)
-        best_sample = 0
-        conversion_rates = self.sample()
-        products_sold = np.random.randint(self.max_products_sold + 1)
-        # TODO: Compute contribution
-        conversion_rates * self.prices * products_sold + contribution
+    def pull(self, results):
+        pulled_arms = []
+        upper_conf = self.update(results)
+        for prod in range(self.n_products):
+            candidates = upper_conf[prod]
+            j = np.random.choice(np.where(candidates == candidates.max())[0])
+            self.pulled_rounds[prod][j] += 1
+            pulled_arms.append(j)
 
-        return best_configuration
+        return pulled_arms
 
     @abstractmethod
     def update(self, results):
@@ -39,3 +42,43 @@ class Learner:
     @abstractmethod
     def sample(self):
         pass
+
+    def compute_nearby_contribution(self, configuration):
+
+        visits_starting_node = np.zeros(self.n_products, self.n_arms)
+        for prod in range(self.n_products):
+            for i in range(self.n_simulations):
+                visited = self.simulation(prod, configuration)
+                for j in range(self.n_products):
+                    if (visited[j] == 1) and j != prod:
+                        visits_starting_node[prod][j] += 1
+
+        return visits_starting_node / self.n_simulations
+
+    def simulation(self, starting_node, pulled_arms):
+        visited = np.zeros(self.n_products)
+        to_visit = [starting_node]
+
+        while to_visit:
+            current_product = to_visit.pop(0)
+            visited[current_product] += 1
+
+            buy = np.random.binomial(1, self.empirical_means[current_product][pulled_arms[current_product]])
+            if not buy:
+                continue
+
+            secondary_1 = self.secondaries[current_product, 0]
+            success_1 = np.random.binomial(1, self.graph_probabilities[current_product, secondary_1])
+            if success_1 and visited[secondary_1] == 0 and secondary_1 not in to_visit:
+                to_visit.append(secondary_1)
+
+            secondary_2 = self.secondaries[current_product, 1]
+            success_2 = np.random.binomial(
+                1, self.lambda_p * self.graph_probabilities[current_product, secondary_2])
+            if success_2 and visited[secondary_2] == 0 and secondary_2 not in to_visit:
+                to_visit.append(secondary_2)
+
+        return visited
+
+
+
--- Step3_v3/UCB.py
+++ Step3_v3/UCB.py
@@ -7,13 +7,14 @@ class UCB(Learner):
 
     def __init__(self, env: Environment):
         super().__init__(env)
-        self.empirical_means = np.zeros((self.n_products, self.n_arms))
         self.confidence = np.ones((self.n_products, self.n_arms)) * np.inf
         self.pulled_rounds = np.zeros((self.n_products, self.n_arms))
+        self.empirical_means = np.zeros(env.n_products, env.n_products)
 
     def update(self, results: RoundData):
 
         configuration = results.configuration
+
         for i in range(self.n_products):
             self.empirical_means[i][configuration[i]] = (self.empirical_means[i][configuration[i]] *
                                                        (self.pulled_rounds[i][configuration[i]]-1) + np.nan_to_num(results.conversions[i]/results.visits[i])) / self.pulled_rounds[i][configuration[i]]
@@ -22,31 +23,25 @@ class UCB(Learner):
                 n_samples = self.pulled_rounds[i][a]
                 self.confidence[i][a] = 500*(2*np.log(self.t+1)/n_samples)**0.5 if n_samples > 0 else np.inf
 
+        from_prod_to_node = self.compute_nearby_contribution(configuration)
 
-    def sample(self, results: RoundData):
+        for prod in range(self.n_products):
+            for temp in range(self.n_products):
+                self.marginal_rewards[prod][configuration[prod]] += self.empirical_means[prod][configuration[prod]] * from_prod_to_node[prod][temp] * \
+                                                                    self.empirical_means[temp][configuration[temp]] * self.max_products_sold[temp][configuration[temp]] * \
+                                                                    self.prices[temp][configuration[temp]]
 
-        superarm = []
-        for i in range(self.n_products):
-            dot = self.empirical_means[i] * self.prices[i]
-            dot = dot * self.products_sold[i]
-            if self.t > 1:
-                print("PREVIOUS PULLED ARM")
-                print(self.previous_pulled[i])
-                print("NODE PRBABILITIES")
-                print(self.node_prob[0][i])
-                dot[self.previous_pulled[i]] += self.marginal_reward[i][self.previous_pulled[i]]
-                print("FINALL DOTTT")
-                print(dot)
-                dot = dot + self.confidence[i]
-            j = np.random.choice(np.where(dot == dot.max())[0])
-            self.pulled_rounds[i][j] += 1
-            superarm.append(j)
-        print("PULLED ROUNDS")
-        print(self.pulled_rounds)
-        self.previous_pulled = superarm
-        print("SUPERARM")
-        print(superarm)
-        return superarm
+
+
+
+    def sample(self):
+
+        upper_conf = self.empirical_means + self.confidence
+        products_sold = np.random.randint(self.max_products_sold + 1)
+        for prod in range(self.n_products):
+            for arm in range(self.n_arms):
+                upper_conf[prod][arm] = upper_conf[prod][arm] * products_sold[prod][arm] * self.prices[prod][arm] + self.marginal_rewards
+        return upper_conf
 
 
 

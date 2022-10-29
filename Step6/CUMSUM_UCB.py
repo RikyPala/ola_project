ce2218f48aec1d784f36f8b41508c6b0b9378fa6
AlessandroImmordino98 <aleimmordino@gmail.com>
2022-10-29 13:14:45 +0200

    Change Detector Step6: initial settings

---
+++ Step6/CUMSUM_UCB.py
@@ -0,0 +1,46 @@
+from CUMSUM import CUMSUM
+from Learner import Learner
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+
+
+class CUMSUM_UCB_Matching(Learner):
+
+    def __init__(self, n_arms, n_rows, n_cols, M = 100, eps = 0.05, h=20, alpha =0.01):
+        super().__init__(n_arms, n_rows, n_cols)
+        self.change_detection = [CUMSUM(M, eps, h) for _ in range(n_arms)] # each arm has a CUMSUM class
+        self.valid_rewards_per_arms = [[] for _ in range(n_arms)]
+        self.detections = [[] for _ in range(n_arms)]
+        self.alpha = alpha  # the probability of picking the arm with the highest upper confidence bound
+
+    def pull_arm(self):
+        if np.random.binomial(1, 1-self.alpha):
+            upper_conf = self.empirical_means+self.confidence
+            upper_conf[np.isinf(upper_conf)] = 1e3
+            row_ind, col_ind = linear_sum_assignment(-upper_conf.reshape(self.n_rows, self.n_cols))
+            return row_ind, col_ind
+        else:
+            costs_random = np.random.randint(0, 10, size = (self.n_rows, self.n_cols))
+            return linear_sum_assignment(costs_random)
+
+
+    def update(self, pulled_arms, rewards):
+        self.t += 1
+        pulled_arm_flat = np.ravel_multi_index(pulled_arms, (self.n_rows, self.n_cols))
+        for pulled_arm, reward in zip(pulled_arm_flat, rewards):
+            if self.change_detection[pulled_arm].update(reward): # If this if is okay it means that there was an abrupt change, so we detect it and we reset all the CUMSUM parameters
+                self.detections[pulled_arm].append(self.t)
+                self.valid_rewards_per_arms[pulled_arm] = []
+                self.change_detection[pulled_arm].reset()
+
+            self.update_observations(pulled_arm, reward)
+            self.empirical_means[pulled_arm] = np.mean(self.valid_rewards_per_arms[pulled_arm])
+        total_valid_samples = sum([len(x) for x in self.valid_rewards_per_arms])
+        for a in range(self.n_arms):
+            n_samples = len(self.valid_rewards_per_arms[a])
+            self.confidence[a] = (2*np.log(total_valid_samples)/n_samples)**0.5 if n_samples >0 else np.inf
+
+    def update_observations(self, pulled_arm, reward):
+        self.rewards_per_arm[pulled_arm].append(reward)
+        self.valid_rewards_per_arms[pulled_arm].append(reward)
+        self.collected_rewards = np.append(self.collected_rewards, reward)

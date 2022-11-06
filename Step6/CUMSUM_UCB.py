e30ca0e878f869a1435b99014427ef2c3a3bb1ad
AlessandroImmordino98 <aleimmordino@gmail.com>
2022-11-06 12:24:59 +0100

    Change Detector Step6

--- Step6/CUMSUM_UCB.py
+++ Step6/CUMSUM_UCB.py
@@ -11,22 +11,18 @@ class CUMSUM_UCB(Learner):
         super().__init__(env)
         self.confidence = np.ones((self.n_products, self.n_arms)) * np.inf
         self.empirical_means = np.zeros((self.n_products, self.n_arms))
-        self.change_detection, self.detections, self.valid_rewards_per_arms = self.init_change_detection(M, eps, h)
+        self.change_detection = self.init_change_detection(M, eps, h)
+        self.detections = [[[] for _ in range(self.n_arms)] for _ in range(self.n_products)]
+        self.valid_rewards_per_arms = [[[] for _ in range(self.n_arms)] for _ in range(self.n_products)]
         self.t = 0
-
 
     def init_change_detection(self, M, eps, h):
         change_detection = [[] for _ in range(self.n_products)]
-        valid_rewards_per_arm = [[] for _ in range(self.n_products)]
-        detections = [[] for _ in range(self.n_products)]
         for prod in range(self.n_products):
             for arm in range(self.n_arms):
-                li = []
                 cum = CUMSUM(M, eps, h)
                 change_detection[prod].append(cum)
-                detections[prod].append(li)
-                valid_rewards_per_arm[prod].append(li)
-        return change_detection, detections, valid_rewards_per_arm
+        return change_detection
 
     def sample(self):
         return self.empirical_means + self.confidence
@@ -41,23 +37,26 @@ class CUMSUM_UCB(Learner):
         for prod in range(self.n_products):
             if self.change_detection[prod][pulled_arm[prod]].update(data, pulled_arm[prod]): # If this if is okay it means that there was an abrupt change, so we detect it and we reset all the CUMSUM parameters
                 self.detections[prod][pulled_arm[prod]].append(self.t)
-                self.valid_rewards_per_arms[prod][pulled_arm[prod]] = []
+                self.valid_rewards_per_arms[prod][pulled_arm[prod]] = [[[] for _ in range(self.n_arms)] for _ in range(self.n_products)]
+                self.pulled_rounds = np.zeros((self.n_products, self.n_arms))
                 self.change_detection[prod][pulled_arm[prod]].reset()
 
             mean_est = data.conversions[prod]/data.visits[prod]
             self.update_observations(pulled_arm, mean_est, prod)
             self.empirical_means[prod][pulled_arm[prod]] = np.mean(self.valid_rewards_per_arms[prod][pulled_arm[prod]])
-            total_valid_samples = 0
+            total_valid_samples = self.compute_total_valid_samples()
             for a in range(self.n_arms):
-                total_valid_samples += sum(self.valid_rewards_per_arms[prod][a])
-            for a in range(self.n_arms):
-                n_samples = 0
-                for arms in range(self.n_arms):
-                    n_samples += len(self.valid_rewards_per_arms[prod][arms])
+                n_samples = self.pulled_rounds[prod][a]
                 self.confidence[prod][a] = (2*np.log(total_valid_samples)/n_samples)**0.5 if n_samples >0 else np.inf
         self.update_marginal_reward(pulled_arm)
 
-    def update_observations(self, pulled_arm, reward, prod):
+    def compute_total_valid_samples(self):
+        total_valid_samples = 0
+        for prod in range(self.n_products):
+            for arm in range(self.n_arms):
+                total_valid_samples += len(self.valid_rewards_per_arms[prod][arm])
+        return total_valid_samples
 
+    def update_observations(self, pulled_arm, reward, prod):
         self.valid_rewards_per_arms[prod][pulled_arm[prod]].append(reward)
 

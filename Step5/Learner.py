aa06672632c987f62c0b2838c0f24102480345ef
GiacomoMosca <10574012@polimi.it>
2022-11-23 11:13:14 +0100

    Step5

--- Step5/Learner.py
+++ Step5/Learner.py
@@ -12,14 +12,13 @@ class Learner:
         self.n_products = env.n_products
         self.n_arms = env.n_arms
         self.prices = env.prices
+        self.avg_products_sold = np.mean(
+            env.max_products_sold * np.expand_dims(env.user_probabilities, axis=(1, 2)), axis=0) / 2.0
         self.lambda_p = env.lambda_p
-        self.graph_probabilities = np.sum(
-            env.graph_probabilities * np.expand_dims(env.user_probabilities, axis=(1, 2)), axis=0)
+        self.alpha_ratios_parameters = np.sum(env.alpha_ratios_parameters, axis=0)
 
-        self.alpha_ratios_data = np.full((self.n_products + 1, 2), 0)
-        self.alpha_ratios_est = np.full(self.n_products + 1, 1. / (self.n_products + 1))
-        self.avg_products_sold_data = np.full((self.n_products, self.n_arms, 2), 0)
-        self.avg_products_sold_est = np.full((self.n_products, self.n_arms), np.inf)
+        self.graph_probabilities_data = np.full((self.n_products, self.n_products, 2), 0)
+        self.graph_probabilities_est = np.full((self.n_products, self.n_products), 1.)
 
         self.n_simulations = 300
         self.marginal_rewards = np.zeros((env.n_products, env.n_arms))
@@ -28,9 +27,9 @@ class Learner:
 
     def pull(self):
         exp_conversion_rates = self.sample()
-        alpha_ratios = np.array([self.alpha_ratios_est[:self.n_products]] * self.n_arms).transpose()
+        alpha_ratios = np.array([self.draw_alpha_ratios()[:self.n_products]] * self.n_arms).transpose()
         exp_rewards = alpha_ratios * \
-            (exp_conversion_rates * self.prices * self.avg_products_sold_est + self.marginal_rewards)
+            (exp_conversion_rates * self.prices * self.avg_products_sold + self.marginal_rewards)
         configuration = np.argmax(exp_rewards, axis=1)
         self.pulled_rounds[np.arange(self.n_products), configuration] += 1
         return configuration
@@ -46,6 +45,11 @@ class Learner:
     @abstractmethod
     def get_means(self):
         pass
+
+    def draw_alpha_ratios(self):
+        alpha = self.alpha_ratios_parameters[:, 0]
+        beta = self.alpha_ratios_parameters[:, 1]
+        return alpha / (alpha + beta)
 
     def compute_reaching_probabilities(self, configuration):
         reaches = np.zeros((self.n_products, self.n_products))
@@ -69,31 +73,24 @@ class Learner:
                 continue
 
             secondary_1 = self.secondaries[current_product, 0]
-            success_1 = np.random.binomial(1, self.graph_probabilities[current_product, secondary_1])
+            success_1 = np.random.binomial(1, self.graph_probabilities_est[current_product, secondary_1])
             if success_1 and visited[secondary_1] == 0 and secondary_1 not in to_visit:
                 to_visit.append(secondary_1)
 
             secondary_2 = self.secondaries[current_product, 1]
-            success_2 = np.random.binomial(
-                1, self.lambda_p * self.graph_probabilities[current_product, secondary_2])
+            # no self.lambda_p (already in estimation)
+            success_2 = np.random.binomial(1, self.graph_probabilities_est[current_product, secondary_2])
             if success_2 and visited[secondary_2] == 0 and secondary_2 not in to_visit:
                 to_visit.append(secondary_2)
 
         return visited
 
-    def update_alpha_ratios(self, results: RoundData):
-        self.alpha_ratios_data[:self.n_products, 0] += results.first_clicks
-        self.alpha_ratios_data[self.n_products, 0] += results.users - np.sum(results.first_clicks)
-        self.alpha_ratios_data[:, 1] += results.users
-        self.alpha_ratios_est = self.alpha_ratios_data[:, 0] / self.alpha_ratios_data[:, 1]
-
-    def update_avg_products_sold(self, configuration, results: RoundData):
-        for prod in range(self.n_products):
-            self.avg_products_sold_data[prod, configuration[prod], 0] += results.sales[prod]
-            self.avg_products_sold_data[prod, configuration[prod], 1] += results.conversions[prod]
-            self.avg_products_sold_est[prod, configuration[prod]] = \
-                self.avg_products_sold_data[prod, configuration[prod], 0] / \
-                self.avg_products_sold_data[prod, configuration[prod], 1]
+    def update_graph_probabilities(self, results: RoundData):
+        self.graph_probabilities_data += results.secondary_visits
+        self.graph_probabilities_est = np.divide(
+            self.graph_probabilities_data[:, :, 0], self.graph_probabilities_data[:, :, 1],
+            out = np.zeros_like(self.graph_probabilities_est),
+            where = self.graph_probabilities_data[:, :, 1] != 0)
 
     def update_marginal_reward(self, configuration):
         reaching_probabilities = self.compute_reaching_probabilities(configuration)
@@ -105,13 +102,12 @@ class Learner:
                 reaching_probabilities[prod] *
                 self.get_means()[idxs, configuration] *
                 self.prices[idxs, configuration] *
-                self.avg_products_sold_est[idxs, configuration]
+                self.avg_products_sold[idxs, configuration]
             )
             n_pulls = self.pulled_rounds[prod, configuration[prod]]
             self.marginal_rewards[prod, configuration[prod]] = \
                 (old_marginal_reward * (n_pulls - 1) + marginal_reward) / n_pulls
 
     def update_estimates(self, configuration, results: RoundData):
-        self.update_alpha_ratios(results)
-        self.update_avg_products_sold(configuration, results)
+        self.update_graph_probabilities(results)
         self.update_marginal_reward(configuration)
---
+++ Step5/_Optimizer.py
@@ -0,0 +1,128 @@
+import numpy as np
+from Environment import Environment, RoundData
+from Learner import Learner
+
+
+class Optimizer:
+
+    def __init__(self, env: Environment, learner: Learner):
+        self.n_products = env.n_products
+        self.n_arms = env.n_arms
+        self.prices = env.prices
+        self.avg_products_sold = np.sum(
+            (env.max_products_sold + 1) / 2 * np.expand_dims(env.user_probabilities, axis=1),
+            axis=0)
+        self.lambda_p = env.lambda_p
+        self.alpha_ratios = np.sum(
+            self.avg_alpha_ratios(env.alpha_ratios_parameters) * np.expand_dims(env.user_probabilities, axis=1),
+            axis=0)
+        self.graph_probabilities = np.sum(
+            env.graph_probabilities * np.expand_dims(env.user_probabilities, axis=(1, 2)),
+            axis=0)
+        self.secondaries = env.secondaries
+
+        self.conversion_rates_data = np.full((self.n_products, self.n_arms, 2), 0.)
+        self.conversion_rates_est = np.full((self.n_products, self.n_arms), 1.)
+
+        self.learner = learner
+        self.prev_reward = 0.
+
+        self.threshold = 0.9
+
+    def avg_alpha_ratios(self, alpha_ratios_parameters):
+        alpha_ratios_avg = alpha_ratios_parameters[:, :, 0] / \
+                           (alpha_ratios_parameters[:, :, 0] + alpha_ratios_parameters[:, :, 1])
+        norm_factors = np.sum(alpha_ratios_avg, axis=1)
+        return (alpha_ratios_avg.T / norm_factors).T
+
+    def draw_starting_page(self, alpha_ratios):
+        product = np.random.choice(self.n_products + 1, p=alpha_ratios)
+        return product
+
+    def compute_node_probabilities(self, configuration):
+        daily_users = 500
+
+        idxs1 = np.arange(self.n_products)
+        idxs2 = self.secondaries[:, 0]
+        idxs3 = self.secondaries[:, 1]
+
+        # all graph probabilities to 0 except those of the secondary products
+        adj_graph_probabilities = np.zeros((self.n_products, self.n_products))
+        adj_graph_probabilities[idxs1, idxs2] = self.graph_probabilities[idxs1, idxs2]
+        adj_graph_probabilities[idxs1, idxs3] = self.graph_probabilities[idxs1, idxs3] * self.lambda_p
+
+        node_visitors = np.zeros(self.n_products)
+
+        for _ in range(daily_users):
+            live_edge_graph = np.random.binomial(
+                1, adj_graph_probabilities * self.conversion_rates_est[idxs1, configuration])
+            start = self.draw_starting_page(self.alpha_ratios)
+            if start == 5:  # competitors' page
+                continue
+            visited = []
+            to_visit = [start]
+
+            while to_visit:
+                current_node = to_visit.pop(0)
+                visited.append(current_node)
+                node_visitors[current_node] += 1
+
+                secondary_1 = self.secondaries[current_node, 0]
+                if live_edge_graph[current_node, secondary_1] and \
+                        secondary_1 not in visited and secondary_1 not in to_visit:
+                    to_visit.append(secondary_1)
+
+                secondary_2 = self.secondaries[current_node, 1]
+                if live_edge_graph[current_node, secondary_2] and \
+                        secondary_2 not in visited and secondary_2 not in to_visit:
+                    to_visit.append(secondary_2)
+
+        node_probabilities = node_visitors / daily_users
+
+        return node_probabilities
+
+    def evaluate_configuration(self, configuration):
+        node_probabilities = self.compute_node_probabilities(configuration)
+        # print(node_probabilities)
+        return np.sum(
+            node_probabilities *
+            self.conversion_rates_est[np.arange(self.n_products), configuration] *
+            self.avg_products_sold *
+            self.prices[np.arange(self.n_products), configuration])
+
+    def update_conversion_rates(self, prod, price, visits, conversions):
+        self.conversion_rates_data[prod, price, 0] += conversions
+        self.conversion_rates_data[prod, price, 1] += visits
+        self.conversion_rates_est[prod, price] = \
+            self.conversion_rates_data[prod, price, 0] / self.conversion_rates_data[prod, price, 1]
+
+    """ OPTIMIZATION """
+
+    def initialize_configuration(self):
+        return (1,) * self.n_products
+
+    def update(self, round_data: RoundData):
+        for prod in range(self.n_products):
+            self.update_conversion_rates(prod, round_data.configuration[prod],
+                                         round_data.visits[prod], round_data.conversions[prod])
+        self.learner.update(round_data)
+        if round_data.reward > self.prev_reward:
+            self.prev_reward = (self.prev_reward + round_data.reward) / 2
+
+    def optimize_round(self):
+        round_best_configuration = (0,)
+        round_best_reward = 0.
+
+        for _ in range(self.n_arms ** self.n_products):
+            configuration = self.learner.pull()
+            # print(configuration)
+            reward = self.evaluate_configuration(configuration)
+            # print(reward)
+            if reward > self.threshold * self.prev_reward:
+                return configuration
+            self.learner.update(configuration, reward)
+            if reward > round_best_reward:
+                round_best_configuration = configuration
+                round_best_reward = reward
+
+        return round_best_configuration

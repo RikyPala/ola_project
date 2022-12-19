313e1cc0169b23dcc3a3b09d6a40774293f368b9
AlessandroImmordino98 <aleimmordino@gmail.com>
2022-12-19 13:03:21 +0100

    modification in the solver

--- Step6/main.py
+++ Step6/main.py
@@ -6,7 +6,6 @@ from CUMSUM_UCB import CUMSUM_UCB
 from SW_UCB import SW_UCB
 from Solver import Solver
 
-n_experiments = 10
 T = 200
 env = NonStationaryEnvironment(T)
 solver = Solver(env)
@@ -23,55 +22,51 @@ for i in range(env.n_phases):
 ucb_cum_sum = CUMSUM_UCB(env)
 ucb_sw = SW_UCB(env)
 
-ucb_cum_sum_rounds = np.zeros((T+1, n_experiments))
-ucb_sw_rounds = np.zeros((T+1, n_experiments))
-optimal_rounds = np.zeros((T+1, n_experiments))
+ucb_cum_sum_rounds = []
+ucb_sw_rounds = []
+optimal_rounds = []
 
-for t in range(n_experiments):
-    for i in range(T):
-        seed = np.random.randint(1, 2 ** 25)
+for i in range(T):
+    seed = np.random.randint(1, 2 ** 30)
 
-        ucb_cum_sum_configuration = ucb_cum_sum.pull()
-        ucb_cum_sum_round_data = env.round(ucb_cum_sum_configuration, seed)
-        ucb_cum_sum.update(ucb_cum_sum_round_data)
-        ucb_cum_sum_rounds[T, n_experiments] = ucb_cum_sum_round_data.reward
+    ucb_cum_sum_configuration = ucb_cum_sum.pull()
+    ucb_cum_sum_round_data = env.round(ucb_cum_sum_configuration, seed)
+    ucb_cum_sum.update(ucb_cum_sum_round_data)
+    ucb_cum_sum_rounds.append(ucb_cum_sum_round_data)
 
-        ucb_sw_configuration = ucb_sw.pull()
-        ucb_sw_round_data = env.round(ucb_sw_configuration, seed)
-        ucb_sw.update(ucb_sw_round_data)
-        ucb_sw_rounds[T, n_experiments] = ucb_sw_round_data.reward
+    ucb_sw_configuration = ucb_sw.pull()
+    ucb_sw_round_data = env.round(ucb_sw_configuration, seed)
+    ucb_sw.update(ucb_sw_round_data)
+    ucb_sw_rounds.append(ucb_sw_round_data)
 
-        current_phase = int(env.t / env.phases_size)
-        optimal_round_data = env.round(optimal_configurations[current_phase], seed, new_round=True)
-        optimal_rounds[T, n_experiments] = optimal_round_data.reward
+    current_phase = int(env.t / env.phases_size)
+    optimal_round_data = env.round(optimal_configurations[current_phase], seed, new_round=True)
+    optimal_rounds.append(optimal_round_data)
 
-        print("\nROUND: " + str(i + 1))
-        print("--------------------CUMSUM UCB---------------------")
-        print("PLAYED: " + str(ucb_cum_sum_configuration))
-        print("REWARD: " + str(ucb_cum_sum_round_data.reward))
-        print("--------------------SW UCB----------------------")
-        print("PLAYED: " + str(ucb_sw_configuration))
-        print("REWARD: " + str(ucb_sw_round_data.reward))
-        print("------------------OPTIMAL-------------------")
-        print("REWARD: " + str(optimal_round_data.reward))
+    print("\nROUND: " + str(i + 1))
+    print("--------------------CUMSUM UCB---------------------")
+    print("PLAYED: " + str(ucb_cum_sum_configuration))
+    print("REWARD: " + str(ucb_cum_sum_round_data.reward))
+    print("--------------------SW UCB----------------------")
+    print("PLAYED: " + str(ucb_sw_configuration))
+    print("REWARD: " + str(ucb_sw_round_data.reward))
+    print("------------------OPTIMAL-------------------")
+    print("REWARD: " + str(optimal_round_data.reward))
 
 print("\n###################################################")
 
-ucb_cum_sum_rewards_avg = []
-ucb_sw_rewards_avg = []
-optimal_rewards_avg = []
+ucb_cum_sum_rewards = []
+ucb_sw_rewards = []
+optimal_rewards = []
 
 for i in range(T):
-    q = np.mean(ucb_cum_sum_rounds[i, :])
-    print("SHAPE MEAN")
-    print(q.shape)
-    ucb_cum_sum_rewards_avg.append(np.mean(ucb_cum_sum_rounds[i, :]))
-    ucb_sw_rewards_avg.append(np.mean(ucb_sw_rounds[i, :]))
-    optimal_rewards_avg.append(np.mean(optimal_rounds[i, :]))
+    ucb_cum_sum_rewards.append(ucb_cum_sum_rounds[i].reward)
+    ucb_sw_rewards.append(ucb_sw_rounds[i].reward)
+    optimal_rewards.append(optimal_rounds[i].reward)
 
-ucb_cum_sum_rewards = np.array(ucb_cum_sum_rewards_avg)
-ucb_sw_rewards = np.array(ucb_sw_rewards_avg)
-optimal_rewards = np.array(optimal_rewards_avg)
+ucb_cum_sum_rewards = np.array(ucb_cum_sum_rewards)
+ucb_sw_rewards = np.array(ucb_sw_rewards)
+optimal_rewards = np.array(optimal_rewards)
 
 plt.figure(0)
 plt.xlabel("t")
